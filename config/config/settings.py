198d822733370017a78db572de31f20691b28a8f
tugazinho07 <a22003736@alunos.ulht.pt>
2022-05-29 21:04:05 +0100

    os fix

--- config/config/settings.py
+++ config/config/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = '/lab8/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'portfolio/static/portfolio/imagens/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
