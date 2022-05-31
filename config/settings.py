3ff5492241a51898bd00b79092449e922e72dd86
tugazinho07 <a22003736@alunos.ulht.pt>
2022-05-31 10:14:14 +0100

    teste

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CLOUDINARY_STORAGE = {
+  'CLOUD_NAME': "portfolio22003736",
+  'API_KEY': "621585929411522",
+  'API_SECRET': "HerYrM2xagKw8nxsheYwvcLSY-E",
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
